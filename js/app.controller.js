29c4e9b7a12dbda1d4e66a71b42f503261da9daf
Chanan Welt <chwelt88@gmail.com>
2025-08-19 01:49:48 +0300

    add rendering for grouping by modification time

--- js/app.controller.js
+++ js/app.controller.js
@@ -301,9 +301,15 @@ function renderLocStats() {
   locService.getLocCountByRateMap().then(stats => {
     handleStats(stats, 'loc-stats-rate');
   });
+
+  debugger;
+  locService.getLocCountByModificationTimeMap().then(stats => {
+    handleStats(stats, 'loc-stats-updated');
+  });
 }
 
 function handleStats(stats, selector) {
+  console.log('Stats:', stats, selector);
   // stats = { low: 37, medium: 11, high: 100, total: 148 }
   // stats = { low: 5, medium: 5, high: 5, baba: 55, mama: 30, total: 100 }
   const labels = cleanStats(stats);
@@ -330,7 +336,7 @@ function handleStats(stats, selector) {
   const style = `background-image: conic-gradient(${colorsStr})`;
   elPie.style = style;
 
-  const ledendHTML = labels
+  const legendHTML = labels
     .map((label, idx) => {
       return `
                 <li>
@@ -342,7 +348,7 @@ function handleStats(stats, selector) {
     .join('');
 
   const elLegend = document.querySelector(`.${selector} .legend`);
-  elLegend.innerHTML = ledendHTML;
+  elLegend.innerHTML = legendHTML;
 }
 
 function cleanStats(stats) {
